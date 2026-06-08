01aa72921e2f9b5a7697c1c2a6bd91a3ee48da24
RJSHS25 <142608744+RJSHS25@users.noreply.github.com>
2026-06-08 13:02:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -212,7 +212,6 @@ else:
     chat_col, nav_col = st.columns([0.85, 0.15])
 
     with nav_col:
-    with nav_col:
     st.markdown("""
     <h4 style='text-align:center;color:#0078d4;'>
     TechM Finance
